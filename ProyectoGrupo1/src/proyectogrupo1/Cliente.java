3475bb23fee20c050659a19a4de5ea347a31c869
LuisM <luismateoarg2012@gmail.com>
2022-08-21 00:59:19 -0500

    Se removieron ciertos headers y se cambió los tipos a los del modelo UML

--- ProyectoGrupo1/src/proyectogrupo1/Cliente.java
+++ ProyectoGrupo1/src/proyectogrupo1/Cliente.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectogrupo1;
 
-/**
- *
- * @author Thomas Tapia
- */
 public class Cliente extends Persona{
     
    private int idCliente;
 
-    Cliente(String cedula, String nombre, String apellido, String telefono, String direccion, String correo) {
+    Cliente(long cedula, String nombre, String apellido, long telefono, String direccion, String correo) {
         super(cedula, nombre, apellido, telefono, direccion, correo);
         //this.idCliente = (int) Math.random(); Crear metodo para obtener id de la base de datos
     }
