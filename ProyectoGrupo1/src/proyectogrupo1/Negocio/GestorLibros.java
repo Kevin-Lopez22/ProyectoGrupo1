b442b48a5043917a1473f0735f5c1da1da808999
Marcelinne <73668598+marcelinnes@users.noreply.github.com>
2022-08-22 22:52:00 -0500

    Clase Libro y Metodos de Gestor

---
+++ ProyectoGrupo1/src/proyectogrupo1/Negocio/GestorLibros.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectogrupo1.Negocio;
+
+/**
+ *
+ * @author marcelinne
+ */
+public class GestorLibros {
+    public void AgregarLibro (Libro libro){
+
+    }
+
+    public void EliminarLibro (Libro libro){
+
+    }
+
+    public void buscarLibro(Libro libro){
+
+    }
+
+    public void agregarEjemplar (Libro libro){
+
+    }
+
+    public void modificarDisponibilidad(Ejemplar ejemplar){
+// nuevo cambio
+    }
+
+}
---
+++ ProyectoGrupo1/src/proyectogrupo1/Negocio/Libro.java
@@ -0,0 +1,28 @@
+package proyectogrupo1.Negocio;
+
+import java.sql.ResultSet;
+
+import proyectogrupo1.ConexionSQL;
+
+import java.sql.Connection;
+
+public class Libro {
+    private int idLibro,stock, paginas ; 
+   
+    private String titulo,autor,ISBN,Stock;
+    
+
+    ConexionSQL conecxion = new ConexionSQL();
+    
+    static ResultSet res;
+
+    public Libro(int idLibro, int stock, int paginas, String titulo, String autor, String iSBN, String stock2) {
+        this.idLibro = idLibro;
+        this.stock = stock;
+        this.paginas = paginas;
+        this.titulo = titulo;
+        this.autor = autor;
+        ISBN = iSBN;
+        Stock = stock2;
+    }
+}
