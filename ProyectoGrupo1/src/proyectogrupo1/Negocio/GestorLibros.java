e513a2e98d3bdd7de6928e718e6b6716dce3d839
Marcelinne <73668598+marcelinnes@users.noreply.github.com>
2022-08-22 21:59:38 -0500

    Agregar metodo Con de Conexion Sql

---
+++ ProyectoGrupo1/src/proyectogrupo1/Negocio/GestorLibros.java
@@ -0,0 +1,25 @@
+package proyectogrupo1.Negocio;
+
+public class GestorLibros {
+    
+    public void AgregarLibro (Libro libro){
+
+    }
+
+    public void EliminarLibro (Libro libro){
+
+    }
+
+    public void buscarLibro(Libro libro){
+
+    }
+
+    public void agregarEjemplar (Libro libro){
+
+    }
+
+    public void modificarDisponibilidad(Ejemplar ejemplar){
+
+    }
+
+}
