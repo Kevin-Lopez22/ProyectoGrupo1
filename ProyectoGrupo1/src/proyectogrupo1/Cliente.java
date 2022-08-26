dc4d872fd30214a05bd12926fd4806b2b05b8844
Usuario <Usuario@DESKTOP-H5VHSUT>
2022-08-25 19:02:42 -0500

    Suspender???? No soporta la base de datos

--- ProyectoGrupo1/src/proyectogrupo1/Cliente.java
+++ ProyectoGrupo1/src/proyectogrupo1/Cliente.java
@@ -4,7 +4,7 @@ public class Cliente extends Persona{
     
    private final int idCliente;
 
-    Cliente(int idCliente, long cedula, String nombre, String apellido, long telefono, String direccion, String correo) {
+    Cliente(int idCliente, String cedula, String nombre, String apellido, long telefono, String direccion, String correo) {
         super(cedula, nombre, apellido, telefono, direccion, correo);
         this.idCliente = idCliente;
         //this.idCliente = (int) Math.random(); Crear metodo para obtener id de la base de datos
