7b7737779ce16d1edcfebc0178b8765f1cd48aab
LuisM <luismateoarg2012@gmail.com>
2022-08-26 23:46:43 -0500

    SqlCrud integrado para Update

--- ProyectoGrupo1/src/proyectogrupo1/GUIActualizar.java
+++ ProyectoGrupo1/src/proyectogrupo1/GUIActualizar.java
@@ -4,17 +4,23 @@
  */
 package proyectogrupo1;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Thomas Tapia
  */
 public class GUIActualizar extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Actualizar
-     */
-    public GUIActualizar() {
+    /* Variables para la actualizacion del cliente */
+    Cliente clientePorModificar;    // Variable auxiliar para modificar y enviar al CRUD
+    SqlCrudCliente sqlCrudCliente;  // Handler para el crud
+    
+    public GUIActualizar(SqlCrudCliente sqlCrudCliente) {
         initComponents();
+        this.sqlCrudCliente = sqlCrudCliente;
     }
 
     /**
@@ -172,43 +178,16 @@ public class GUIActualizar extends javax.swing.JFrame {
         
         variableString = txtEditar.getText(); //Esto tambien que se actualice en la base de datos y se cambie el textArea pilas mateo
         
-    }//GEN-LAST:event_btnAceptarActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        // Antes de este comentario actualizen el objeto clientePorModificar\
+        // como sea pertinente
+        // La documentación da mas detalles de como opera el SqlCrudCliente
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUIActualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUIActualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUIActualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUIActualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            sqlCrudCliente.update(clientePorModificar);
+        } catch (SQLException ex) {
+            // TODO: Control de excepciones
+            Logger.getLogger(GUIActualizar.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GUIActualizar().setVisible(true);
-            }
-        });
-    }
+    }//GEN-LAST:event_btnAceptarActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAceptar;
