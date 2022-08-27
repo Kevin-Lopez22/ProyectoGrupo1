d85c032c4c305328ebaa327216befc82b75eb336
LuisM <luismateoarg2012@gmail.com>
2022-08-27 00:11:39 -0500

    BuilderCambiado para poder compartir conexiones

--- ProyectoGrupo1/src/proyectogrupo1/GUIGestorCliente.java
+++ ProyectoGrupo1/src/proyectogrupo1/GUIGestorCliente.java
@@ -18,10 +18,10 @@ public class GUIGestorCliente extends javax.swing.JFrame {
     /**
      * Creates new form GUIGestorCliente
      */
-    SqlCrud<Cliente,Integer> sqlCrudCliente;
-    public GUIGestorCliente() {
+    SqlCrudCliente sqlCrudCliente;
+    public GUIGestorCliente(java.sql.Connection connection) {
         initComponents();
-        sqlCrudCliente = new SqlCrudCliente(ConexionSQL.getConexion());
+        sqlCrudCliente = new SqlCrudCliente(connection);
     }
 
     /**
@@ -131,41 +131,7 @@ public class GUIGestorCliente extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btnRegistrarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUIGestorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUIGestorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUIGestorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUIGestorCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GUIGestorCliente().setVisible(true);
-            }
-        });
-    }
 
     void registrarCliente(Cliente cliente){
         try {
