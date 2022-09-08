b836e198b1e3269d37dfcb2ee2fc152ff67e9efc
Steven Quispe <78981969+stevenq9@users.noreply.github.com>
2022-09-08 18:57:54 -0500

    Feat: Add Gestión de Préstamos

---
+++ ProyectoGrupo1/src/proyectogrupo1/proyectogrupo1/ConexionSQL.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectogrupo1;
+
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author denni
+ */
+public class ConexionSQL {
+    //trabajando con jre17
+    
+    public static Connection getConexion(){
+        String conexionUrl = "jdbc:sqlserver://localhost:1433; "//puerto por defecto
+                + "database = baseBiblioteca;"
+                + "user = sa;"
+                + "password = 12345;"//pilas la clave 
+                + "loginTimeout = 40;"; //tiempo para la conexion 
+        try{
+            Connection con = DriverManager.getConnection(conexionUrl);
+            //JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+            return con;
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Conexion fallida: "+ex.toString());
+            return null;
+        }
+    }
+    
+}
