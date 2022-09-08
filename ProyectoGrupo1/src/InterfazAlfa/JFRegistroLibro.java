0baa18863f9f3fbe924314e106ec2a8628a4def4
JavierRevelo <shavi.sprint@gmail.com>
2022-09-07 23:02:22 -0500

    Cambio en las interfaces 2

--- ProyectoGrupo1/src/InterfazAlfa/JFRegistroLibro.java
+++ ProyectoGrupo1/src/InterfazAlfa/JFRegistroLibro.java
@@ -1,27 +1,30 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
 package InterfazAlfa;
 
 import java.awt.event.KeyEvent;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import proyectogrupo1.Negocio.GestorLibros;
 import proyectogrupo1.Negocio.Libro;
 
+/**
+ *
+ * @author Det-Pc
+ */
 public class JFRegistroLibro extends javax.swing.JFrame {
-    
+
     Libro libro;
     GestorLibros gestorLibro;
-    
-    public JFRegistroLibro() throws SQLException {
+    public JFRegistroLibro() {
         initComponents();
         this.setLocationRelativeTo(this);
         libro = new Libro();
         gestorLibro = new GestorLibros();
-        //jCBPais.setModel(reservaCliente.llenarComboboxCli("SELECT PAIS FROM PAIS"));        
     }
 
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -35,7 +38,6 @@ public class JFRegistroLibro extends javax.swing.JFrame {
         jTFPaginas = new javax.swing.JTextField();
         jBIngresarClienteRes = new javax.swing.JButton();
         jBRegresar = new javax.swing.JButton();
-        jLabel6 = new javax.swing.JLabel();
         jTFTituloLibro = new javax.swing.JTextField();
         jTFAutorLibro = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
@@ -47,46 +49,38 @@ public class JFRegistroLibro extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("REGISTRO DE CLIENTES");
         addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 formKeyPressed(evt);
             }
         });
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Verdana", 1, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("REGISTRO DE LIBROS");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 40, 510, 40));
 
         jLabel2.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Id_Libro:");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, 270, -1));
 
         jLabel3.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Titulo:");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 160, 300, -1));
 
         jLabel4.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Autor:");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 380, -1));
 
         jLabel5.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("N Páginas ");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 340, 350, -1));
-        getContentPane().add(jTFIDLIbro, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 120, 220, -1));
+
+        jTFIDLIbro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTFIDLIbroActionPerformed(evt);
+            }
+        });
 
         jTFPaginas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTFPaginasActionPerformed(evt);
             }
         });
-        getContentPane().add(jTFPaginas, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 340, 220, -1));
 
         jBIngresarClienteRes.setBackground(new java.awt.Color(153, 153, 153));
         jBIngresarClienteRes.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
@@ -97,7 +91,6 @@ public class JFRegistroLibro extends javax.swing.JFrame {
                 jBIngresarClienteResActionPerformed(evt);
             }
         });
-        getContentPane().add(jBIngresarClienteRes, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 410, 200, -1));
 
         jBRegresar.setBackground(new java.awt.Color(153, 153, 153));
         jBRegresar.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
@@ -108,33 +101,22 @@ public class JFRegistroLibro extends javax.swing.JFrame {
                 jBRegresarActionPerformed(evt);
             }
         });
-        getContentPane().add(jBRegresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 410, 210, -1));
-
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Fondo2REGISTRO.jpg"))); // NOI18N
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 470));
-        getContentPane().add(jTFTituloLibro, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 160, 220, -1));
-        getContentPane().add(jTFAutorLibro, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 200, 220, -1));
 
         jLabel7.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
         jLabel7.setText("ISBN");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, 350, -1));
-        getContentPane().add(jTFIsbnLibro, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 250, 220, -1));
 
         jLabel8.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
-        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
         jLabel8.setText("Stock");
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 350, -1));
 
         jTFStock.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTFStockActionPerformed(evt);
             }
         });
-        getContentPane().add(jTFStock, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 290, 220, -1));
 
         jMenu2.setText("Edit");
 
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0));
         jMenuItem1.setText("Salir");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -147,8 +129,97 @@ public class JFRegistroLibro extends javax.swing.JFrame {
 
         setJMenuBar(jMenuBar1);
 
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 596, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 28, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(30, 30, 30)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 510, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(20, 20, 20)
+                            .addComponent(jTFIDLIbro, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(290, 290, 290)
+                            .addComponent(jTFTituloLibro, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(290, 290, 290)
+                            .addComponent(jTFAutorLibro, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(290, 290, 290)
+                            .addComponent(jTFIsbnLibro, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(290, 290, 290)
+                            .addComponent(jTFStock, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addGap(50, 50, 50)
+                                    .addComponent(jBIngresarClienteRes, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addGap(50, 50, 50)
+                                    .addComponent(jBRegresar, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                    .addGap(40, 40, 40)
+                                    .addComponent(jTFPaginas, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGap(0, 28, Short.MAX_VALUE)))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 424, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 9, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(40, 40, 40)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel2)
+                        .addComponent(jTFIDLIbro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(10, 10, 10)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel3)
+                        .addComponent(jTFTituloLibro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(10, 10, 10)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jTFAutorLibro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel4))
+                    .addGap(20, 20, 20)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jTFIsbnLibro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel7))
+                    .addGap(10, 10, 10)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel8)
+                        .addComponent(jTFStock, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(20, 20, 20)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel5)
+                        .addComponent(jTFPaginas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(40, 40, 40)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jBIngresarClienteRes)
+                        .addComponent(jBRegresar))
+                    .addGap(0, 9, Short.MAX_VALUE)))
+        );
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTFPaginasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTFPaginasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTFPaginasActionPerformed
 
     private void jBIngresarClienteResActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBIngresarClienteResActionPerformed
         int idLibro = Integer.parseInt(jTFIDLIbro.getText());
@@ -157,7 +228,7 @@ public class JFRegistroLibro extends javax.swing.JFrame {
         String isbn = jTFIsbnLibro.getText();
         int stock = Integer.parseInt(jTFStock.getText());
         int paginas = Integer.parseInt(jTFPaginas.getText());
-       
+
         libro = new Libro(idLibro, titulo, autor, isbn, paginas, stock);
         boolean respuesta = gestorLibro.agregarLibro(libro);
         if(respuesta == false){
@@ -172,10 +243,6 @@ public class JFRegistroLibro extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_jBRegresarActionPerformed
 
-    private void jTFPaginasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTFPaginasActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTFPaginasActionPerformed
-
     private void jTFStockActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTFStockActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTFStockActionPerformed
@@ -185,10 +252,12 @@ public class JFRegistroLibro extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
-        if (evt.getKeyCode()==KeyEvent.VK_ESCAPE) {
-            System.exit(0);
-        }
+     
     }//GEN-LAST:event_formKeyPressed
+
+    private void jTFIDLIbroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTFIDLIbroActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTFIDLIbroActionPerformed
 
     /**
      * @param args the command line arguments
@@ -217,17 +286,11 @@ public class JFRegistroLibro extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                try {
-                    new JFRegistroLibro().setVisible(true);
-                } catch (SQLException ex) {
-                    Logger.getLogger(JFRegistroLibro.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                new JFRegistroLibro().setVisible(true);
             }
         });
     }
@@ -240,7 +303,6 @@ public class JFRegistroLibro extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JMenu jMenu2;
