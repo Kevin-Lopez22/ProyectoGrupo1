4d2f85f21ad0f669d633a3dc92ca43effba416f3
Betsabe Amaguai <Usuario@DESKTOP-H5VHSUT>
2022-09-08 09:06:51 -0500

    Se agrego funciones

--- ProyectoGrupo1/src/proyectogrupo1/GUIActualizar.java
+++ ProyectoGrupo1/src/proyectogrupo1/GUIActualizar.java
@@ -168,19 +168,21 @@ public class GUIActualizar extends javax.swing.JFrame {
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
         String cedula = "";
-        JTextArea textArea = new JTextArea(cedula);
+     
+        
         cedula = txtCedula.getText();
             try {
             List<Cliente> clientes = sqlCrudCliente.read(cedula);
             
             if(clientes.isEmpty()){
-                JOptionPane.showMessageDialog(null, "No se ha encontrado el cliente especificado"); // Ahi le cambian
+                JOptionPane.showMessageDialog(null, "No se ha encontrado el cliente especificado" ); // Ahi le cambian
                 clientePorModificar = null;
                 return;
                 }
             if(clientes.size()==1){
                 JOptionPane.showMessageDialog(null, "Cliente encontrado!"); // Ahi le cambian
                 clientePorModificar = clientes.get(0);
+                add(txaInformacion);
                 }
             }
             catch (SQLException ex) {
@@ -189,7 +191,7 @@ public class GUIActualizar extends javax.swing.JFrame {
         }
         
         /*Aplicar logica para ingresar a la base de datos e imprimir en el textArea*/
-        txaInformacion.setVisible(true);
+
     }//GEN-LAST:event_btnBuscarActionPerformed
 
     private void cmbEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbEditarActionPerformed
